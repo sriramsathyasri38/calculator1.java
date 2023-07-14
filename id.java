5ee8b016a5866988c1a9b878b98df608dcb7ee1e
sriramsathyasri38 <136477780+sriramsathyasri38@users.noreply.github.com>
2023-07-14 20:19:36 +0530

    Create id.java

---
+++ id.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+// Define the calculator interface
+interface Calculator {
+    double add(double x, double y);
+    double subtract(double x, double y);
+    double multiply(double x, double y);
+    double divide(double x, double y);
+}
+
+// Implement the calculator interface
+class BasicCalculator implements Calculator {
+    @Override
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+    @Override
+    public double subtract(double x, double y) {
+        return x - y;
+    }
+
+    @Override
+    public double multiply(double x, double y) {
+        return x * y;
+    }
+
+    @Override
+    public double divide(double x, double y) {
+        return x / y;
+    }
+}
+
+// Main class to run the calculator
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the calculator
+        Calculator calculator = new BasicCalculator();
+
+        // Read input from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Perform calculations
+        double sum = calculator.add(num1, num2);
+        double difference = calculator.subtract(num1, num2);
+        double product = calculator.multiply(num1, num2);
+        double quotient = calculator.divide(num1, num2);
+
+        // Display the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        scanner.close();
+    }
+}
